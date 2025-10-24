75c6f81d20505c26176c4f0713e0bec9a8e5141b
Djasn <djasnos06@outlook.com>
2025-10-23 23:24:12 -0500

    Added web player and 360 degree protractor

---
+++ src/test/java/com/physicsvisualtools/inertiaandrotation/PhysicsToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.physicsvisualtools.inertiaandrotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicsToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
